7515405eb8119cede8790a227ee68205756e30e7
jskl <jskl@itu.dk>
2021-09-09 19:29:44 +0200

    asdA

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ass0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ unittest/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using System.IO;
+
+namespace unittest
+{
+    public class UnitTest1
+    {
+      //  private const string Expected = "Hello World!";
+        [Fact]
+        public void Test1()
+        {
+          
+        }
+    }
+}
